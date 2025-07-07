f926a8933f0b65918356f87c8d4dfd1cb18e728a
Clément Nicolas <clement92nicolas@gmail.com>
2025-07-07 11:25:13 +0200

    create a firebase config and index.vue

---
+++ firebase.config.ts
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBARJeop_KsQ5N2XXbC0eG4lx2TLCJhtps",
+    authDomain: "nuxt-ev.firebaseapp.com",
+    projectId: "nuxt-ev",
+    storageBucket: "nuxt-ev.firebasestorage.app",
+    messagingSenderId: "366155786751",
+    appId: "1:366155786751:web:2d378cc2f454058b3c5fca"
+  };
+
+// Initialiser Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialiser Firestore
+const db = getFirestore(app);
+
+// Exporter l'instance de Firestore pour l'utiliser dans d'autres fichiers
+export default db;
