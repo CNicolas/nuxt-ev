35c73d34ab337fa1b21b744ba89ffd3cbf687e63
Clément Nicolas <clement92nicolas@gmail.com>
2025-07-07 13:31:31 +0200

    trying github action to gh pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  compatibilityDate: "2025-05-15",
   devtools: { enabled: false },
-  css: ['~/assets/css/main.css'],
-  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/ui'],
-  plugins: [
-    '~/plugins/firebase.ts'
-  ],
-})
+  css: ["~/assets/css/main.css"],
+  modules: ["@nuxt/eslint", "@nuxt/icon", "@nuxt/ui"],
+  plugins: ["~/plugins/firebase.ts"],
+});
